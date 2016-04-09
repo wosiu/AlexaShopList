0d5219837fa7023a8b1a5be72221f88f2aec70bd
Michał Woś <mw792829570@gmail.com>
2016-04-09 04:53:07 +0200

    base

--- src/main/java/Engine/ResolveProduct.java
+++ src/main/java/Engine/ResolveProduct.java
@@ -1,20 +1,25 @@
 package Engine;
 
+import org.apache.log4j.Logger;
 import pl.edu.mimuw.students.wosiu.scraper.ConnectionException;
 import pl.edu.mimuw.students.wosiu.scraper.alexa.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ResolveProduct {
-
+	private static final Logger logger = Logger.getLogger(ResolveProduct.class);
 	private TranslateExecutor translator = null;
+	private ProductScrapExecutor scraper = null;
 
 	public ResolveProduct() throws ConnectionException {
 		translator = new TranslateExecutor();
+		scraper = new ProductScrapExecutor();
 	}
 
 	public void resolve(String productName) {
 		// translate
+		logger.info("Resolve product: " + productName);
 		String productNameTr = translator.translate(productName);
 
 		List offers = scrapOffers(productNameTr);
@@ -22,21 +27,24 @@ public class ResolveProduct {
 		List best = agregateOffers(offers);
 
 		store(best);
-	}
 
-	private void store(List best) {
-		// TODO !
-	}
-
-	private List agregateOffers(List offers) {
-		// TODO !
-		return null;
+		// TODO? send feedback
 	}
 
 	private List scrapOffers(String productNameTr) {
-		// TODO !
-		return null;
+		logger.info("Scrap: " + productNameTr);
+		// TODO in scraper...
+		return scraper.scrap(productNameTr);
 	}
 
+	private List agregateOffers(List offers) {
+		logger.info("Agregate offers, size: " + offers.size());
+		// TODO ! This is mock!
+		return Arrays.asList(offers.get(0));
+	}
 
+	private void store(List best) {
+		logger.info("Store, size: " + best.size());
+		// TODO !
+	}
 }
